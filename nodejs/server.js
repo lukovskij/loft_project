87c08d91b73d38fe4b96422fbe5423928e13a35a
alexander.kravchuk <alexander.kravchuk@nakitel.com>
2017-11-01 13:47:45 +0200

    edit catch in index.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,4 +4,21 @@ const routing = require('./routing');
 
 let server = new http.Server(function(req, res){
 
-})
+		 var jssonString = '';
+		 res.setHeader('Content-Type', 'application/json');
+
+		 req.on('data', (data) => {
+
+        jssonString += data;
+
+		 });
+
+		 req.on('end', () => {
+
+         routing.define(req, res, jssonString);
+
+		 });
+
+});
+
+server.listen(8000, 'localhost');
