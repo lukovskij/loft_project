fc379ae29247608bf95c2b0fca614a88cae1a022
lukovskij <kravchuk1995@ukr.net>
2017-11-04 19:28:50 +0300

    add styles and symple html in app. add server and fb api

--- server.js
+++ server.js
@@ -2,14 +2,25 @@ let express = require('express');
 
 let app = express();
 
+let port = process.env.PORT || 3000;
+
+
+app.use(express.static('app'))
+
+app.use('/app', express.static(__dirname + '/app'));
+
 app.get('/', function(req, res) {
 
-	res.sendFile('index.html', {root: __dirname});
+	res.sendFile('index.html', {root: __dirname + '/app/'});
 
 });
 
-app.listen(8000);
+app.listen(port, function(){
 
-console.log('server started');
+	console.log('listen port ', port);
+
+});
+
+
 
 
